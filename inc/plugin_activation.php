6108bd037f88c5973ba5ad8a76f26d015047c970
nayeem-droitlab <nayeem@droitlab.com>
2021-12-15 10:58:13 +0600

    Update plugin_activation.php

--- inc/plugin_activation.php
+++ inc/plugin_activation.php
@@ -30,87 +30,79 @@ add_action( 'tgmpa_register', 'carspa_resister_required_plugin' );
  * TGM_Plugin_Activation class constructor.
  */
 function carspa_resister_required_plugin() {
-    /*
-     * Array of plugin arrays. Required keys are name and slug.
-     * If the source is NOT from the .org repo, then source is also required.
-     */
-    $plugins = array(
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
 
-        array(
-            'name'      => esc_html__( 'Elementor', 'carspa' ),
-            'slug'      => 'elementor',
-            'required'  => true,
-        ),
+		array(
+			'name'      => esc_html__( 'Elementor', 'carspa' ),
+			'slug'      => 'elementor',
+			'required'  => true,
+		),
 
-        array(
-            'name'               => esc_html__( 'Droit Addons For Elementor', 'carspa' ),
-            'slug'               => 'droit-elementor-addons',
-            'required'           => true,
-        ),
+		array(
+			'name'               => esc_html__( 'Droit Addons For Elementor', 'carspa' ),
+			'slug'               => 'droit-elementor-addons',
+			'required'           => true,
+		),
 
-        array(
-            'name'               => esc_html__( 'carspa Core', 'carspa' ), // The plugin name.
-            'slug'               => 'carspa-core', // The plugin slug (typically the folder name).
-            'source'             => 'http://preview.droitthemes.net/wp/carspa/downloadfile/carspa-core.zip', // The plugin source.
-            'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-            'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-            'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-            'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-            'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-            'version'            => '1.0'
-        ),
-        
-        array(
-            'name'               => esc_html__( 'Advanced Custom Fields-pro', 'carspa' ), // The plugin name.
-            'slug'               => 'advanced-custom-fields-pro', // The plugin slug (typically the folder name).
-            'source'             => 'https://wpplugin.droitthemes.net/common/advanced-custom-fields-pro.zip', // The plugin source.
-            'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-            'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-            'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-            'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-            'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-        ),
+		array(
+			'name'               => esc_html__( 'Carspa Core', 'carspa' ), // The plugin name.
+			'slug'               => 'carspa-core', // The plugin slug (typically the folder name).
+			'source'             => 'http://preview.droitthemes.com/wp/carspa/downloadfile/carspa-core.zip', // The plugin source.
+			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+		array(
+			'name'               => esc_html__( 'Redux Framework', 'carspa' ),
+			'slug'               => 'redux-framework',
+			'required'           => true,
+		),
+		array(
+			'name'               => esc_html__( 'Advanced Custom Fields', 'carspa' ),
+			'slug'               => 'advanced-custom-fields',
+			'required'           => true,
+		),
 
-        array(
-            'name'               => esc_html__( 'Redux Framework', 'carspa' ),
-            'slug'               => 'redux-framework',
-            'required'           => true,
-        ),
+		array(
+			'name'      => esc_html__( 'Metform', 'carspa' ),
+			'slug'      => 'metform',
+			'required'  => true,
+		),
 
-        array(
-            'name'      => esc_html__( 'Metform', 'carspa' ),
-            'slug'      => 'metform',
-            'required'  => true,
-        ),
+		array(
+			'name'      => esc_html__( 'One Click Demo Import', 'carspa' ),
+			'slug'      => 'one-click-demo-import',
+			'required'  => false,
+		),
+	);
 
-        array(
-            'name'      => esc_html__( 'One Click Demo Import', 'carspa' ),
-            'slug'      => 'one-click-demo-import',
-            'required'  => false,
-        ),
-    );
+	/*
+	 * Array of configuration settings. Amend each line as needed.
+	 *
+	 * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+	 * strings available, please help us make TGMPA even better by giving us access to these translations or by
+	 * sending in a pull-request with .po file(s) with the translations.
+	 *
+	 * Only uncomment the strings in the config array if you want to customize the strings.
+	 */
+	$config = array(
+		'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'themes.php',            // Parent menu slug.
+		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
 
-    /*
-     * Array of configuration settings. Amend each line as needed.
-     *
-     * TGMPA will start providing localized text strings soon. If you already have translations of our standard
-     * strings available, please help us make TGMPA even better by giving us access to these translations or by
-     * sending in a pull-request with .po file(s) with the translations.
-     *
-     * Only uncomment the strings in the config array if you want to customize the strings.
-     */
-    $config = array(
-        'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
-        'default_path' => '',                      // Default absolute path to bundled plugins.
-        'menu'         => 'tgmpa-install-plugins', // Menu slug.
-        'parent_slug'  => 'themes.php',            // Parent menu slug.
-        'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-        'has_notices'  => true,                    // Show admin notices or not.
-        'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-        'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-        'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-        'message'      => '',                      // Message to output right before the plugins table.
-    );
-
-    tgmpa( $plugins, $config );
+	tgmpa( $plugins, $config );
 }

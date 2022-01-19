527b67ffddc7c1493919a602545cffed6f3fc536
Abdur Rahaman <79562059+dlab10@users.noreply.github.com>
2022-01-19 12:27:28 +0600

    update

--- inc/plugin_activation.php
+++ inc/plugin_activation.php
@@ -51,7 +51,7 @@ function carspa_resister_required_plugin() {
 		array(
 			'name'               => esc_html__( 'Carspa Core', 'carspa' ), // The plugin name.
 			'slug'               => 'carspa-core', // The plugin slug (typically the folder name).
-			'source'             => 'http://preview.droitthemes.net/wp/carspa/downloadfile/carspa-core.zip', // The plugin source.
+			'source'             => 'http://carspa.droitlab.com/downloadfile/carspa-core.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.

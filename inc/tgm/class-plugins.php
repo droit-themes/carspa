b185fd4614ed35dc8ae8c45ea0ba9a28a0ecef21
Omar Faruk <omarfaruk2y2@gmail.com>
2025-02-17 16:43:26 +0600

    update

--- inc/tgm/class-plugins.php
+++ inc/tgm/class-plugins.php
@@ -51,7 +51,7 @@ class carspa_Register_Plugins {
 			array(
 				'name'               => esc_html__( 'Carspa Core', 'carspa' ), // The plugin name.
 				'slug'               => 'carspa-core', // The plugin slug (typically the folder name).
-				'source'             => 'https://dlcarspa.droitlab.com/downloadfile/carspa-core_1.0.4.zip', // The plugin source.
+				'source'             => 'https://dlcarspa.droitlab.com/downloadfile/carspa-core_1.0.5.zip', // The plugin source.
 				'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 				'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 				'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
